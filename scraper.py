695ae08ee3838783c2e48b21790bdc32c57512aa
bnorman18 <bdn1@rice.edu>
2016-08-27 21:31:14 -0700

    Update scraper.py
    Adds each line of song to one cell of excel file. Now need to loop through multiple related artists to get more data (possibly another website for this relation?)

--- scraper.py
+++ scraper.py
@@ -1,17 +1,23 @@
 from lxml import html
 import requests
+import csv
 
 
-def getSongLyrics(link):
+def getSongLyrics(link, wr):
     page = requests.get(link)
     tree = html.fromstring(page.content)
-    print tree.xpath('//div[@class="lyricbox"]/text()')
+    whole_song=tree.xpath('//div[@class="lyricbox"]/text()')
+    print "This is song is length %d" % len(whole_song)
+    for song_line in whole_song:
+        wr.writerows([[song_line]])
+
 
 def getAllArtistLyrics(artist):
     root = 'http://lyrics.wikia.com'
     artist = '/wiki/' + artist
 
-
+    resultFile = open("songfile.csv",'wb')
+    wr = csv.writer(resultFile, dialect='excel')
     page = requests.get(root+artist)
     tree = html.fromstring(page.content)
 
@@ -24,11 +30,11 @@ def getAllArtistLyrics(artist):
                 a = b.getchildren()[0]
                 if (a != None):
                     href = a.get('href')
-                    getSongLyrics(root + href)
+                    getSongLyrics(root + href, wr)
 
 
 def main():
-    artist = 'Steely_Dn'
+    artist = 'Whiskey_Myers'
     getAllArtistLyrics(artist)
 
 main()
